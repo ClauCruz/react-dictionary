a2df5f4b0ce58ae16a11a24124c472586bf6fe2e
Cluadiacg <claudiacg2607@gmail.com>
2022-09-24 17:28:12 -0500

    New react project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Dictionary</p>
+        <a href="#" className="btn btn-primary">
+          Hello
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
